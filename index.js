2765ef08d2268a29708baa3f140bd9cf50207ced
Jb Person <homeaccount@Jbs-MacBook-Air.local>
2023-01-06 10:04:22 -0500

    task 3 done

--- index.js
+++ index.js
@@ -121,12 +121,15 @@ class Car {
 */
 
 class Lambdasian {
-  construction(arg){
+  constructor({name, age, location}){
     this.name = name; 
     this.age = age; 
     this.location = location; 
   }
   
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
 }
 
 /*
